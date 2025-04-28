286487adc6556617e0dea24dcb941f45592a0b45
JosephRidge <josephridge18@gmail.com>
2025-04-28 03:43:11 +0300

    STRUCTURE

---
+++ lib/pages/my_app.dart
@@ -0,0 +1,23 @@
+import 'package:flutter/material.dart';
+import 'package:state/pages/my_home_page.dart';
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      ),
+      home: MyHomePage(
+        title: "Dealing with States",
+        onChanged: (value) {
+          print("Count value has increased to : $value");
+        },
+      ),
+    );
+  }
+}
